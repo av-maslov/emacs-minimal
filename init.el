0f3733ce410e78915e288fc819fd28337ce54180
Alexandr <alexandr.maslov@jyu.fi>
2015-05-22 10:17:52 +0300

    color theme selection function is fixed

--- init.el
+++ init.el
@@ -1,13 +1,14 @@
- (require 'epa-file)
+(require 'epa-file)
 (epa-file-enable)
 
-;; turn off beep
+;; Turn off beep
 (setq visible-bell 1) 
 (require 'ido)
 (ido-mode t)
 
 (global-linum-mode 1)
 
+;; Key bindings
 (global-set-key (kbd "C-l") 'goto-line)
 (global-set-key [S-up] 'backward-paragraph)      ;; Jump to previous paragraph
 (global-set-key [S-down] 'forward-paragraph)     ;; Jump to next paragraph
@@ -22,7 +23,7 @@
 ;; Uncomment this to increase font size
 (set-face-attribute 'default nil :height 110)
 
-(add-to-list 'load-path (get-full-path "plugins/"))
+;; (add-to-list 'load-path (get-full-path "plugins/"))
 
 ;; Color themes
 (add-to-list 'custom-theme-load-path (get-full-path "themes") )
@@ -30,18 +31,21 @@
 (add-to-list 'load-path (get-full-path "themes/emacs-color-theme-solarized/"))
 (add-to-list 'custom-theme-load-path (get-full-path "themes/emacs-color-theme-solarized/"))
 
-(setq frame-background-mode 'light)
-(set-frame-parameter nil 'background-mode 'dark)
+;;(setq frame-background-mode 'light)
+;;(setq frame-background-mode 'dark)
+;; (set-frame-parameter nil 'background-mode 'dark)
 (load-theme 'solarized t)
 (enable-theme 'solarized)
 
 (defun light ()
   (interactive)
+  (set-frame-parameter nil 'background-mode 'light)
   (setq frame-background-mode 'light)
   (enable-theme 'solarized))
 
 (defun dark ()
   (interactive)
+  (set-frame-parameter nil 'background-mode 'dark)
   (setq frame-background-mode 'dark)
   (enable-theme 'solarized))
 
