d590c8ffa0b9d7985340923794fd4da3c9f311dd
Alexandr <av-maslov@users.noreply.github.com>
2015-08-07 14:24:13 +0200

    timer hook

--- customizations/utils.el
+++ customizations/utils.el
@@ -4,3 +4,6 @@
 
 (setq org-timer-default-timer 20)
 ;; org-timer-set timer - run timer
+(add-hook 'org-clock-in-hook (lambda ()
+   (if (not org-timer-current-timer) 
+	  (org-timer-set-timer '(16)))))
