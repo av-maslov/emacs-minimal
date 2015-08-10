a40d534886708e989ef92e0112ceb2763ef7aeb6
Alexandr <av-maslov@users.noreply.github.com>
2015-08-10 16:13:19 +0200

    return to the last position

--- customizations/utils.el
+++ customizations/utils.el
@@ -7,3 +7,11 @@
 (add-hook 'org-clock-in-hook (lambda ()
    (if (not org-timer-current-timer) 
 	  (org-timer-set-timer '(16)))))
+	 
+	 
+;; mark position to return
+;; ctrl-f7 go back
+;; http://www.tonyballantyne.com/EmacsWritingTips.html
+(global-set-key (kbd "<f7>") 'org-mark-ring-push)
+(global-set-key (kbd "C-<f7>") 'org-mark-ring-goto)
+
