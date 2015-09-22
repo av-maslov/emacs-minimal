151688e27c9a4818558f34309a6974af993517f5
Alexander M <alexandr.v.maslov@gmail.com>
2015-09-22 20:18:49 +0200

    Deft plugin

--- customizations/packages/deftplugin.el
+++ customizations/packages/deftplugin.el
@@ -1,5 +1,5 @@
 ;; http://jblevins.org/projects/deft/
 (require 'deft)
-(setq deft-extensions '("txt" "tex" "org"))
+(setq deft-extensions '("txt" "tex" "org" "md"))
 (setq deft-directory "D:/MyTemp/gitlocal/CheatSheets/")
 (setq deft-recursive t)
