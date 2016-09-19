957ecd4419e00e81b14a33dbe81a7ece6ff954e1
Alexandr <maslov314@gmail.com>
2016-09-19 11:58:46 +0200

    Monokai color theme

--- customizations/colors.el
+++ customizations/colors.el
@@ -4,11 +4,18 @@
 (add-to-list 'load-path (get-full-path "themes/emacs-color-theme-solarized/"))
 (add-to-list 'custom-theme-load-path (get-full-path "themes/emacs-color-theme-solarized/"))
 
+(add-to-list 'custom-theme-load-path (get-full-path "themes/monokai-emacs/"))
+(load-theme 'monokai t)
+
 ;;(setq frame-background-mode 'light)
 ;;(setq frame-background-mode 'dark)
 ;; (set-frame-parameter nil 'background-mode 'dark)
 (load-theme 'solarized t)
 (enable-theme 'solarized)
+
+(defun monokai ()
+  (interactive)
+  (enable-theme 'monokai))
 
 (defun light ()
   (interactive)
