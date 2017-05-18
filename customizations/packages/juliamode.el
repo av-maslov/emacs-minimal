a5c15b74b249fb0a4f39a30e60549a2063a13e7a
Alexandr <av-maslov@users.noreply.github.com>
2017-05-18 12:34:28 +0300

    Create juliamode.el

---
+++ customizations/packages/juliamode.el
@@ -0,0 +1,6 @@
+;; Start Julia mode
+;; https://github.com/JuliaEditorSupport/julia-emacs
+;; Download and unzip into the plugins folder
+;;(add-to-list 'load-path (get-full-path "plugins/julia-emacs/"))
+;;(require 'julia-mode)
+;; End Julia mode 
